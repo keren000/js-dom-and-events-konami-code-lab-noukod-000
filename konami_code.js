3c884f0f47c67bd2086cac86fd32eaae2f8c58fc
Keren-Happuc Fleurimond <keren@noukod.com>
2019-03-01 15:36:20 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -5,7 +5,7 @@ function init() {
   let index = 0;
 
   // This is the function that would be invoked by the event listener.
-  function onKeyDownHandler(e) {
+document.body.addEventListener('keydown',function (e) {
     const key = parseInt(e.detail || e.which);
 
     if (key === alphabet[index]) {
@@ -19,5 +19,5 @@ function init() {
     } else {
       index = 0;
     }
-  }
+  });
 }
