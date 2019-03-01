9d50afb1331a3c34d1fdce548e74d1577b840ed8
Keren-Happuc Fleurimond <keren@noukod.com>
2019-03-01 15:38:27 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  document.body.addEventListener('keydown', onKeyDownHandler {
+  document.body.addEventListener('keydown', onKeyDownHandler) {
 }
   // Write your JavaScript code inside the init() function
   let index = 0;
